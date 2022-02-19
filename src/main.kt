8a8116d8d9b453dad627b3d9d1d9aa89c0681143
Wanjiru-Kamau <njirukmaureen@gmail.com>
2022-02-19 15:20:29 +0300

    assignment2

---
+++ src/main.kt
@@ -0,0 +1,23 @@
+fun main(){
+name()
+    modulus(15,2)
+    addition(4,5,6,8)
+    intrestingfact()
+}
+fun name(){
+    var name = "maureen"
+    println("hello " +name)
+}
+fun modulus(a:Int , b:Int) {
+    var modulus = a % b
+    println(modulus)
+}
+fun addition(a:Int , b:Int , c:Int , d:Int){
+    var addition = (a+b+c+d)
+    println(addition)
+
+}
+fun intrestingfact(){
+    var fact="i love travelling"
+    println(fact)
+}
